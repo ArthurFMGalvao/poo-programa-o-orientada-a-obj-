1ea843c76a48a0dee9c011277675cffa94fda1e3
ArthurFMGalvao <147092714+ArthurFMGalvao@users.noreply.github.com>
2024-04-24 16:12:45 -0300

    Add files via upload

---
+++ atividade avaliativa 01 poo/atividade.ts
@@ -0,0 +1,62 @@
+class Menu{
+    private ItemMenu: [];
+
+    imprimirMenu(){
+        return 
+    }
+
+}
+
+
+class ItemMenu{
+    private opcao: string;
+    private textoDaOpcao: string;
+}
+
+
+
+
+
+
+
+class Coliseu{
+    private chefe: Monstro
+
+}
+
+
+
+class Monstro{
+    private hp: number;
+    private fq: number;
+    private fd: number;
+
+    receberDano(danoRecebido: number){
+
+        
+    }
+
+
+
+}
+
+
+class Equipamentos{
+    private nome: string;
+    private aumentoDeAtaque: number;
+    private aumentoDeDefesa: number;
+}
+
+
+
+class Lutador{
+    private Hp: number;
+    private Mp: number;
+     
+}
+
+class Jogo{
+    private Menu: Menu;
+    private Lutador: Lutador;
+    private Coliseu: Coliseu;
+}
